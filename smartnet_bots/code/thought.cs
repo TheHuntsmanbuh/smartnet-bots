49f2eb49e61e40cac28bedace2981fa14a4a1ccc
TheHuntsmanbuh <158553946+TheHuntsmanbuh@users.noreply.github.com>
2024-09-11 23:14:22 +1000

    ai does stuff

--- smartnet_bots/code/thought.cs
+++ smartnet_bots/code/thought.cs
@@ -20,6 +20,8 @@ public static class Thoughts
 	//constantly updated variables go here eg: nearby bots or current health\\
 	public static int NearbyFriends = 0; public static int NearbyEnimies = 0;
 	public static int CurrentHealth = 0;
+	public static Vector3 myposition;
+	public static Vector3 targetposition;
 
 	//bot related variables eg:"closest friend" or "closest enemy"\\
 	public static string LastPersonToHurtMe = BNULL; public static int DistantToDamage = 0;
@@ -30,10 +32,10 @@ public static class Thoughts
 	public static bool Panicked = false; // if set to true the ai will go into a custom panicked script
 	[Property] public static bool AgressiveMode { get; set; }
 
-	//movement states and variables eg: "IsAnimating" or "InFreefall"\\
-	public static bool IsAnimating = false; // if the bot is in a custom animation this will set to TRUE 
+    //movement states and variables eg: "IsAnimating" or "InFreefall"\\
+    public static bool IsAnimating = false; // if the bot is in a custom animation this will set to TRUE 
 	public static bool InFreefall = false; // if the bot is falling this will be set to true so no other actions are performed midair
-	
+	public static int SearchRadius = 128;
 }
 
 
@@ -48,30 +50,47 @@ public sealed class Think : Component
 	{
 		while ( Thoughts.InFreefall == false )
 		{
-			var mypos = Agent.Transform.Position;
-			var test = Scene.Directory.FindByName( "Cube" ).First();
-			var p = test.Transform.Position;
-			Agent.MoveTo( p );
+
+			Agent.MoveTo( Thoughts.targetposition );
 			await Task.DelaySeconds(1);
-			p = test.Transform.Position;
-			mypos = Agent.Transform.Position;
 
-			while ( p != mypos )
-			{
+			
 
-			}
+			
 
 		}
-		await Task.DelaySeconds( 15 );
+		await Task.DelaySeconds( 1 );
 	}
-
+	async Task Roam()
+	{
+		Vector3? movto = Scene.NavMesh.GetRandomPoint( Agent.Transform.Position, 500f );
+		Agent.MoveTo( movto.Value );
+		
+		while ( movto != Thoughts.myposition )
+		{
+			await Task.DelaySeconds( 5 );
+		}
+		await Task.DelaySeconds( 5 );
+	}
 	protected override void OnStart()
 	{
 
+		Roam();
 
-		MainAiLoop();
 
 
+	}
+
+	protected override void OnUpdate()
+	{
+		Thoughts.myposition = Agent.Transform.Position;
+		var target = Scene.Directory.FindByName( "Cube" ).First();
+		Thoughts.targetposition = target.Transform.Position;
+		if ( target == null )
+		{
+			
+
+		}
 	}
 }
 
